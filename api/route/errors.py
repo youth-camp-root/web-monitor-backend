b46f5b16c9f2cfde5faebbc2b2b6508796902b26
RoseBud13 <xiongkaijie13@gmail.com>
2022-08-18 19:12:45 +0800

    feat(error): update error releated apis with swagger docs and made them seperated file

---
+++ api/route/errors.py
@@ -0,0 +1,120 @@
+from flask import Blueprint, jsonify, request, abort
+from bson import ObjectId
+
+from api.util.utils import failResponseWrap, successResponseWrap
+from api.model.models import *
+from ..util.data_process import get_errors_overview, get_error_detail_overview, get_past_days
+
+api = Blueprint('errors', __name__, url_prefix='/error')
+
+
+@api.route('/', methods=['GET'])
+def get_errors():
+    """返回所有Errors
+    ---
+    tags:
+        - Errors
+    parameters:
+        - name: category
+          in: query
+          type: string
+          required: false
+        - name: userid
+          in: query
+          type: string
+          required: false
+
+    description:
+        包含 error issue 所有信息的列表
+        可以按照category或者user来查询
+    """
+    category = request.args.get('category')
+    userID = request.args.get('userid')
+
+    if category:
+        errors = ErrorData.objects(category=category)
+    elif userID:
+        errors = ErrorData.objects(user=ObjectId(userID))
+    else:
+        errors = ErrorData.objects
+
+    if not errors:
+        return failResponseWrap(msg='Error not found')
+    else:
+        return successResponseWrap(errors)
+
+
+@api.route('/issue/<errorID>', methods=['GET'])
+def getErrorInfo(errorID):
+    """查询某个 Error Issue
+    ---
+    tags:
+        - Errors
+    parameters:
+        - name: errorID
+          in: path
+          type: string
+          required: true
+    """
+    if not errorID:
+        return failResponseWrap(msg='Error ID needed')
+    
+    try:
+        errorIssue = ErrorData.objects(_id=ObjectId(errorID)).first()
+
+        if not errorIssue:
+            return failResponseWrap(msg='Error not found')
+
+        error_details = get_error_detail_overview(errorIssue['errorType'], errorIssue['originURL'])
+
+        error_info = {
+            'name': errorIssue['errorType'],
+            'info': errorIssue,
+            'details': error_details
+        }
+
+        return successResponseWrap(error_info)
+
+    except Exception as e:
+        print(e)
+        return failResponseWrap(msg='Internal Error')
+
+
+@api.route('/issues/error-overview', methods=['GET'])
+def error_overview():
+    """返回过去30天Errors按照类别分类的统计信息
+    ---
+    tags:
+        - Errors
+    """
+    data = get_errors_overview()
+    date_list = get_past_days(30)
+
+    return successResponseWrap({'dateList': date_list, 'data': data})
+
+
+@api.route('/issues/list', methods=['GET'])
+def error_list():
+    """返回所有Errors信息并包含过去14天的统计信息
+    ---
+    tags:
+        - Errors
+    """
+    error_issue_list = []
+
+    date_list = get_past_days(30)
+    errors = ErrorData.objects
+    errors_count = ErrorData.objects.count()
+
+    for error in errors:
+        error_details = get_error_detail_overview(error['errorType'], error['originURL'])
+        error_info = {
+            'name': error['errorType'],
+            'info': error,
+            'details': error_details
+        }
+        error_issue_list.append(error_info)
+
+    return successResponseWrap({'list': error_issue_list, 'dateList': date_list, 'total': errors_count})
+
+
