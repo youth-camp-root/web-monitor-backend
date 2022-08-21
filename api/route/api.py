555a5b8499b1044faf97cdbae4aee63995a95e45
Liny777 <1093742432@qq.com>
2022-08-21 14:17:07 +0800

    feat(overview,useraction): add routes

--- api/route/api.py
+++ api/route/api.py
@@ -10,12 +10,16 @@ from api.route.user import api as user_api
 from api.route.mock_api import api as mock_api
 from api.route.performance import api as performance_api
 from api.route.errors import api as error_api
+from api.route.useraction import api as useraction_api
+from api.route.overview import api as overview_api
 
 api = Blueprint('api', __name__, url_prefix='/api')
 api.register_blueprint(user_api)
 api.register_blueprint(mock_api)
 api.register_blueprint(performance_api)
 api.register_blueprint(error_api)
+api.register_blueprint(useraction_api)
+api.register_blueprint(overview_api)
 
 
 @api.route('/request', methods=['GET'])
