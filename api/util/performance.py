9008a8faaa088a26a30c3a707560f40c5ed14c4b
Sioncovy <1298184727@qq.com>
2022-08-22 06:52:30 +0800

    fix(performance): 修改概览返回格式

--- api/util/performance.py
+++ api/util/performance.py
@@ -5,17 +5,20 @@ from statistics import *
 def get_page_detail_overview(pageUrl, date_range):
     pages = PageLoad.objects.filter(pageUrl=pageUrl, timestamp__gte=date_range[0] + ' 00:00:00', timestamp__lte=date_range[len(date_range)-1] + ' 23:59:59')
 
-    pageData = []
-    pageData = pageData + [[{
-      'name': 'good',
-      'value': pages.filter(DOMReady__lte=100.0).count()
-    },{
-      'name': 'medium',
-      'value': pages.filter(DOMReady__gte=100.0).filter(DOMReady__lt=300.0).count()
-    },{
-      'name': 'bad',
-      'value': pages.filter(DOMReady__gte=300.0).count()
-    }]]
+    webVitail = [
+        {
+          'name': 'good',
+          'value': pages.filter(DOMReady__lte=100.0).count()
+        },{
+          'name': 'medium',
+          'value': pages.filter(DOMReady__gte=100.0).filter(DOMReady__lt=300.0).count()
+        },{
+          'name': 'bad',
+          'value': pages.filter(DOMReady__gte=300.0).count()
+        }
+      ]
+    
+    
 
     chartData = []
     for name in ['DOMReady', 'stayDuration']:
@@ -32,37 +35,43 @@ def get_page_detail_overview(pageUrl, date_range):
         'contentData': dataArr,
         'xData': date_range
       })
-    pageData = pageData + [chartData]
 
     tagsData = []
     tagsData = pages.distinct('user')
-    pageData = pageData + [tagsData]
-    return pageData
+
+    errorsData = []
+    errorsData = ErrorData.objects.filter(originURL=pageUrl, timestamp__gte=date_range[0] + ' 00:00:00', timestamp__lte=date_range[len(date_range)-1] + ' 23:59:59').count()
+
+    return {
+      'webVitals': webVitail,
+      'chartData': chartData,
+      'tagsData': tagsData,
+      'errorCount': errorsData
+    }
   
 def get_api_detail_overview(apiUrl, date_range):
     apis = RequestData.objects.filter(targetURL=apiUrl, timestamp__gte=date_range[0] + ' 00:00:00', timestamp__lte=date_range[len(date_range)-1] + ' 23:59:59')
-    apiData = []
-    apiData = apiData + [[{
-      'name': 'good',
-      'value': apis.filter(httpDuration__lte=3000.0).count()
-    },{
-      'name': 'medium',
-      'value': apis.filter(httpDuration__gte=3000.0).filter(httpDuration__lt=7000.0).count()
-    },{
-      'name': 'bad',
-      'value': apis.filter(httpDuration__gte=7000.0).count()
-    }]]
 
-    chartData = []
     dataArr = []
     for day in date_range:
         data = RequestData.objects.filter(targetURL=apiUrl, timestamp__gte=day + ' 00:00:00', timestamp__lte=day + ' 23:59:59').count()
         dataArr.append(data)
 
-    chartData.append({
-      'titleText': '访问量',
-      'contentData': dataArr,
-      'xData': date_range
-      })
-    apiData = apiData + chartData
-    return apiData
+    return {
+      'apiVitals': [{
+        'name': 'good',
+        'value': apis.filter(httpDuration__lte=3000.0).count()
+      },{
+        'name': 'medium',
+        'value': apis.filter(httpDuration__gte=3000.0).filter(httpDuration__lt=7000.0).count()
+      },{
+        'name': 'bad',
+        'value': apis.filter(httpDuration__gte=7000.0).count()
+      }],
+      'chartData': {
+        'titleText': '访问量',
+        'contentData': dataArr,
+        'xData': date_range
+      }
+    }
+    
